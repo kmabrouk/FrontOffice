8d64268be04081fc7d3166f7a0c95bcc760375f4
Aminsty <m_h_c@outlook.fr>
2022-11-05 12:31:47 +0100

    Deleting multer

--- back-end/routes/demandes.js
+++ back-end/routes/demandes.js
@@ -1,27 +1,8 @@
 let express = require("express");
-const multer = require("multer");
 const demandeFuncs = require("../controllers/demandeController.js");
 
-const fileStorageEngine = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "uploads/");
-  },
-  filename: (req, file, cb) => {
-    cb(null, Date.now() + "-" + file.originalname);
-  },
-});
-
-const uploadStorage = multer({ storage: fileStorageEngine });
 let router = express.Router();
 
-
-// Single file
-router
-  .route("/upload/single")
-  .post(uploadStorage.single("file"), (req, res) => {
-    console.log(req.file);
-    return res.send("Single file");
-  });
 
 router.route("/").get(demandeFuncs.getDemandes);
 router.route("/:id").get(demandeFuncs.getDemandeById).delete(demandeFuncs.DeleteDemande);
